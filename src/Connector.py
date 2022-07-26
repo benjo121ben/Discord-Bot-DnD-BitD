5e06406a62f2a5a500353f02bea29ce4f9ae9138
Benji <81331374+benjo121ben@users.noreply.github.com>
2022-07-26 11:31:36 +0200

    connecter reload and load rewritten

--- src/Connector.py
+++ src/Connector.py
@@ -4,16 +4,14 @@ from random import seed
 from datetime import datetime
 
 bot = None
-game_list = {}
-seed(datetime.now().timestamp())
 ext_base_path = "src.ext."
+seed(datetime.now().timestamp())
 
 
 def start_connection(_command_prefix, _bot_token):
     global bot
     bot = commands.Bot(_command_prefix)
     load_extensions(bot)
-
 
     @bot.event
     async def on_ready():
@@ -22,16 +20,19 @@ def start_connection(_command_prefix, _bot_token):
 
     @bot.command(name="r")
     async def reload(ctx):
-        bot.reload_extension("src.Clocks.clock_commands")
+        load_extensions(bot, reload=True)
         await ctx.send("reloaded")
 
     print("starting up bot")
     bot.run(_bot_token)
 
 
-def load_extensions(bot):
+def load_extensions(_bot, reload=False):
     def load_ext(extension):
-        bot.load_extension(ext_base_path + extension)
+        if reload:
+            _bot.reload_extension(ext_base_path + extension)
+        else:
+            _bot.load_extension(ext_base_path + extension)
 
     print("\nloading extensions\n")
     load_ext("Clocks.clock_commands")
