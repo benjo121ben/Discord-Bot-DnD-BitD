c87df83f54872ee38cd81705d8e0a53c11ce5e48
Benji <81331374+benjo121ben@users.noreply.github.com>
2022-12-02 21:51:19 +0100

    unit test WIP

--- unittests/test_CommandFunctions.py
+++ unittests/test_CommandFunctions.py
@@ -1,37 +1,21 @@
 from unittest import TestCase
 from src.ext.Campaign import save_file_management as save_manager, CommandFunctions as commands
-from decohints import decohints
-from functools import wraps
 
 
 unit_test_save_file_name = "unit_test_save"
 
 
-def setup_test_file():
-    if save_manager.check_savefile_existence(unit_test_save_file_name):
-        save_manager.remove(unit_test_save_file_name)
-        print("deleted existing unit test savefile")
-    save_manager.load(unit_test_save_file_name)
-
-
-def close_test_file():
-    save_manager.remove(unit_test_save_file_name)
-
-
-@decohints
-def unit_test_save_file_wrapper(unit_test_to_wrap):
-    @wraps(unit_test_to_wrap)
-    def wrapped_func(*args, **kwargs):
-        setup_test_file()
-        value = unit_test_to_wrap(*args, **kwargs)
-        close_test_file()
-        return value
-    return wrapped_func
-
-
 class TestCommandFunctions(TestCase):
-    @unit_test_save_file_wrapper
+    def setUp(self) -> None:
+        if save_manager.check_savefile_existence(unit_test_save_file_name):
+            save_manager.remove(unit_test_save_file_name)
+            print("deleted existing unit test savefile")
+        save_manager.load(unit_test_save_file_name)
+
+    def tearDown(self) -> None:
+        save_manager.remove(unit_test_save_file_name)
+
     def test_log(self):
-        print(commands.add_char("test", "etst", 20))
+        print(commands.add_char("test", "test", 20))
         print(commands.log())
         assert save_manager.check_savefile_existence(unit_test_save_file_name)
