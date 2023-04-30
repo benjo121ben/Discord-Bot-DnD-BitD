b65d299b63b5024df8057f2b390f0086c5023d01
benjo121ben <benjamin.wenger00@gmail.com>
2023-04-30 05:57:43 +0200

    removed leftover wiki entry constant labels

--- src/ext/BladesUtility/EntryLabels.py
+++ src/ext/BladesUtility/EntryLabels.py
@@ -15,7 +15,6 @@ FORMAT_FIELD_TEXT_LABEL = "format_field_text"
 # entry property labels
 TITLE_LABEL = 'title'
 DESCRIPTION_LABEL = 'description'
-EXAMPLE_LABEL = 'example'
 LOAD_LABEL = 'load'
 IMAGE_LABEL = 'image'
 PLAYBOOK_LABEL = 'playbook'
@@ -24,22 +23,7 @@ FIELD_HEADER_LABEL = 'field_header'
 FIELD_TEXT_LABEL = 'field_text'
 FIELD_INLINE_LABEL = 'field_inline'
 FIELD_SUBLIST_LABEL = 'field_sublist'
-LIST_LABEL = 'list'
-PLAYBOOK_ITEMS_LABEL = 'items'
-PLAYBOOK_FRIENDS_LABEL = 'friends_and_enemies'
 
 # properties unique to entry groups
 ENTRYGROUP_VALUES_LABEL = "group_values"
 
-# properties unique to created items
-CREATION_TYPE_LABEL = 'type'
-CREATION_DRAWBACK_LABEL = 'drawback'
-CREATION_USES_LABEL = 'uses'
-CREATION_TIER_LABEL = 'tier'
-
-# wiki entry category labels
-CAT_ITEMS_LABEL = 'standard_items'
-CAT_PLAYBOOK_ITEMS_LABEL = 'playbook_items'
-CAT_CREATIONS_LABEL = 'sample-creations'
-CAT_PLAYBOOKS_LABEL = 'playbooks'
-CAT_STATS_LABEL = 'stats'
