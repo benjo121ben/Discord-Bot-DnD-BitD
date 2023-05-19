fb27a265851aba6bd0e34c316514c32987cce0da
benjo121ben <benjamin.wenger00@gmail.com>
2023-05-19 14:14:03 +0200

    no default sort on normal rolls

--- src/ext/BladesUtility/RollUtilityCog.py
+++ src/ext/BladesUtility/RollUtilityCog.py
@@ -16,7 +16,7 @@ class RollUtilityCog(commands.Cog):
     @option(name="dice_amount", description="the amount of dice you want to roll")
     @option(name="dice_size", description="the size of your dice")
     @option(name="sorted_dice", description="do you wish to sort your dice?")
-    async def types_roll(self, ctx, dice_amount: int, dice_size: int, sorted_dice: bool = True):
+    async def types_roll(self, ctx, dice_amount: int, dice_size: int, sorted_dice: bool = False):
         try:
             if sorted_dice:
                 await all_size_roll_sorted(ctx, dice_amount, dice_size)
